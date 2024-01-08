ed77ed14d14d82609c0579ae59068b491841b2ab
Keenu <32987931+xullul@users.noreply.github.com>
2024-01-08 23:37:14 +0700

    javascript 2023 day1

---
+++ 2023/Javascript/day1.js
@@ -0,0 +1,38 @@
+const fs = require('node:fs');
+
+
+const filepath = '../inputs/day1.txt';
+const number_regex = /(\d)+/;
+
+function getNumberFromLine(line) {
+	const numbers = [];
+
+	for (const character of line) {
+		if (number_regex.test(character)) {
+			numbers.push(character);
+		}
+	}
+
+	if (numbers.length >= 2) {
+		const firstNumber = numbers[0];
+		const lastNumber = numbers[numbers.length - 1];
+		return `${firstNumber}${lastNumber}` | 0;
+	} else if (numbers.length == 1) {
+		return `${numbers[0]}${numbers[0]}` | 0;
+	} else {
+		return 0;
+	}
+}
+
+fs.readFile(filepath, 'utf-8', (error, data) => {
+	if (error) throw error;
+
+	let total = 0;
+	const lines = data.split(/\r?\n/);
+
+	for (const line of lines) {
+		total +=  getNumberFromLine(line);
+	}
+
+	console.log(total);
+});
